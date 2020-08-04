2048d4709b5cf7f67d7280b6b50ba23eabf0d009
Mateusz Prusaczyk <mateusz.prusaczyk@codete.com>
2020-08-04 09:44:22 +0200

    problem 1 solved

--- index.js
+++ index.js
@@ -5,7 +5,24 @@
  */
 exports.example = () => 'hello world';
 
-exports.stripPrivateProperties = () => {};
+/**
+ * 
+ * @param {string[]} propertiesToExclude 
+ * @param {any[]} objects 
+ */
+exports.stripPrivateProperties = (propertiesToExclude, objects) => {
+  return objects.map((object) => {
+    return Object.entries(object)
+      .filter(([key, value]) => !propertiesToExclude.includes(key))
+      .reduce((object, [key, value]) => {
+        return {
+          ...object,
+          [key]: value
+        }
+      }, {});
+  });
+};
+
 exports.excludeByProperty = () => {};
 exports.sumDeep = () => {};
 exports.applyStatusColor = () => {};
